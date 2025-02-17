41697c3ac9095e7f8db7fc5e6d7716953159b88a
TheFirstPaige1 <Dazzanator2@gmail.com>
2025-02-17 19:25:14 +1030

    playing with things to get a feel for it

--- template.ts
+++ template.ts
@@ -1,5 +1,5 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
-  ns.tprint("Hello Remote API!");
+  ns.tprint("this is a testing change to see if we set this up right");
 }
